69a7fe3abd71fb47b33a339546b27fcce8d9a26a
Robert Austin <rob@jugglerdigital.com>
2021-10-18 15:04:26 +1000

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
 const plugin = require('tailwindcss/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme');
+const themeStyle = require('./content/data/style.json');
 
 module.exports = {
     presets: [require('@stackbit/components/styles/default/tailwind.default.config.js')],
     mode: 'jit',
     purge: {
         enabled: true,
-        content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@stackbit/components/{base,layouts,components}/**/*.{js,ts,jsx,tsx}'],
+        content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@stackbit/components/{base,layouts,components}/**/*.{js,ts,jsx,tsx}', './content/**'],
         safelist: ['colors-a', 'colors-b', 'colors-c', 'colors-d', 'colors-e', 'colors-f', 'colors-g', 'colors-h', 'colors-i']
     },
     darkMode: false,
     theme: {
         extend: {
             colors: {
-                // primary: '#207bea'
+                body: themeStyle.body,
+                headline: themeStyle.headline,
+                primary: themeStyle.primary,
+                secondary: themeStyle.secondary,
+                neutral: themeStyle.neutral,
+                complementary: themeStyle.complementary,
+                'complementary-alt': themeStyle['complementary-alt'],
+                info: themeStyle.info,
+                success: themeStyle.success,
+                warning: themeStyle.warning
             }
         }
     },
